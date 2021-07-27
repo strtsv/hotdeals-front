545fa4d63209f6fbb7a2f9237338f020faa2ae92
jsusilo <enr@bk.ru>
2021-07-27 20:21:20 +0500

    Careers AvailableVacancies

---
+++ src/components/Careers/AvailableVacancies.js
@@ -0,0 +1,165 @@
+import React from "react";
+
+class AvailableVacancies extends React.Component {
+  render() {
+    return (
+      <section className="section section-lg bg-gray-1 text-center">
+        <div className="container">
+          <h3>Available Vacancies</h3>
+          <div className="row row-30 row-lg-45">
+            <div className="col-md-6 col-lg-4">
+              {}
+              <article className="career-classic">
+                <h4 className="career-classic-title">
+                  <a href="single-job.html">Group Guide</a>
+                </h4>
+                <div className="career-classic-divider" />
+                <ul className="career-classic-list">
+                  <li>
+                    <span className="icon mdi mdi-calendar-clock" />
+                    <span>Full Time</span>
+                  </li>
+                  <li>
+                    <span className="icon mdi mdi-map-marker" />
+                    <span>San Diego</span>
+                  </li>
+                </ul>
+                <p>
+                  Tabes velums, tanquam camerarius tata. A falsis, urbs superbus
+                  xiphias. Nunquam examinare abnoba. Humani generiss observare,
+                  tanquam tus.
+                </p>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              {}
+              <article className="career-classic">
+                <h4 className="career-classic-title">
+                  <a href="single-job.html">Travel Agent</a>
+                </h4>
+                <div className="career-classic-divider" />
+                <ul className="career-classic-list">
+                  <li>
+                    <span className="icon mdi mdi-calendar-clock" />
+                    <span>Full Time</span>
+                  </li>
+                  <li>
+                    <span className="icon mdi mdi-map-marker" />
+                    <span>San Diego</span>
+                  </li>
+                </ul>
+                <p>
+                  Superbus uria recte attrahendams bursa est. Sunt cottaes
+                  quaestio camerarius, dexter assimilatioes. Lotus, noster
+                  classiss recte locus de pius, flavum bubo. Diatrias sunt
+                  particulas de flavum ignigena.
+                </p>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              {}
+              <article className="career-classic">
+                <h4 className="career-classic-title">
+                  <a href="single-job.html">Booking Manager</a>
+                </h4>
+                <div className="career-classic-divider" />
+                <ul className="career-classic-list">
+                  <li>
+                    <span className="icon mdi mdi-calendar-clock" />
+                    <span>Full Time</span>
+                  </li>
+                  <li>
+                    <span className="icon mdi mdi-map-marker" />
+                    <span>San Diego</span>
+                  </li>
+                </ul>
+                <p>
+                  Era de salvus fluctus, promissio lumen! Magister de castus
+                  aonides, magicae adelphis! Cum calcaria credere, omnes lunaes
+                  reperire grandis, mirabilis accolaes.
+                </p>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              {}
+              <article className="career-classic">
+                <h4 className="career-classic-title">
+                  <a href="single-job.html">Tour Consultant</a>
+                </h4>
+                <div className="career-classic-divider" />
+                <ul className="career-classic-list">
+                  <li>
+                    <span className="icon mdi mdi-calendar-clock" />
+                    <span>Full Time</span>
+                  </li>
+                  <li>
+                    <span className="icon mdi mdi-map-marker" />
+                    <span>San Diego</span>
+                  </li>
+                </ul>
+                <p>
+                  Byssuss potus! Finiss peregrinatione in antenna! Cum zirbus
+                  ire, omnes rectores demitto rusticus, orgiaes. Cum fluctus
+                  velum, elogiumes imperium castus.
+                </p>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              {}
+              <article className="career-classic">
+                <h4 className="career-classic-title">
+                  <a href="single-job.html">Personal Travel Expert</a>
+                </h4>
+                <div className="career-classic-divider" />
+                <ul className="career-classic-list">
+                  <li>
+                    <span className="icon mdi mdi-calendar-clock" />
+                    <span>Full Time</span>
+                  </li>
+                  <li>
+                    <span className="icon mdi mdi-map-marker" />
+                    <span>San Diego</span>
+                  </li>
+                </ul>
+                <p>
+                  Heu, luna! Sunt racanaes imitari bassus, lotus aususes.
+                  Orexiss mori, tanquam velox extum. Raptus, nobilis bullas
+                  etiam aperto de placidus, albus ventus.
+                </p>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              {}
+              <article className="career-classic">
+                <h4 className="career-classic-title">
+                  <a href="single-job.html">Product Manager</a>
+                </h4>
+                <div className="career-classic-divider" />
+                <ul className="career-classic-list">
+                  <li>
+                    <span className="icon mdi mdi-calendar-clock" />
+                    <span>Full Time</span>
+                  </li>
+                  <li>
+                    <span className="icon mdi mdi-map-marker" />
+                    <span>San Diego</span>
+                  </li>
+                </ul>
+                <p>
+                  Ecce, secundus devirginato! Cum ignigena nocere, omnes
+                  tumultumquees acquirere superbus, clemens victrixes. Compater,
+                  domina, et extum.
+                </p>
+              </article>
+            </div>
+          </div>
+          <a className="button button-lg button-primary button-winona" href="#">
+            Join Us
+          </a>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default AvailableVacancies;
