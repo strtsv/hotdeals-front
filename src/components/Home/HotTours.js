ed5645b444ef0b48b2642d7937b7e0eeee44918b
jsusilo <enr@bk.ru>
2021-08-04 00:02:54 +0500

    Home hot tours

---
+++ src/components/Home/HotTours.js
@@ -0,0 +1,219 @@
+import React from "react";
+
+import i1 from "../../assets/images/thumbnail-simple-1-270x200.jpg";
+import i2 from "../../assets/images/thumbnail-simple-2-270x200.jpg";
+import i3 from "../../assets/images/thumbnail-simple-3-270x200.jpg";
+import i4 from "../../assets/images/thumbnail-simple-4-270x200.jpg";
+import i5 from "../../assets/images/thumbnail-simple-5-270x200.jpg";
+import i6 from "../../assets/images/thumbnail-simple-6-270x200.jpg";
+import i7 from "../../assets/images/thumbnail-simple-7-270x200.jpg";
+import i8 from "../../assets/images/thumbnail-simple-8-270x200.jpg";
+
+class HotTours extends React.Component {
+  render() {
+    return (
+      <section className="section section-lg text-center">
+        <div className="container">
+          <h3 className="wow-outer">
+            <span className="wow slideInUp">Hot Tours</span>
+          </h3>
+          <p className="wow-outer">
+            <span className="text-width-1 wow slideInDown">
+              At Hot Deals, we provide a wide variety of tours for our clients.
+              No matter what kind of travel experience you enjoy the most, we
+              surely have what you need.
+            </span>
+          </p>
+          <div className="row row-offset-big row-30 row-lg-50 justify-content-center">
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i1}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$680</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">5 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Benidorm, Spain</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i2}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$420</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">7 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Mauritius Island, Africa</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i3}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$540</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">5 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Mediterranean Coast</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i4}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$610</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">7 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Paris, France</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i5}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$380</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">7 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Venice, Italy</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i6}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$1280</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">14 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Cairo, Egypt</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i7}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$625</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">7 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Bangkok, Thailand</a>
+                </h5>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3 wow-outer">
+              {}
+              <article className="thumbnail-simple wow slideInLeft">
+                <a className="thumbnail-simple-link" href="single-tour.html">
+                  <img
+                    className="thumbnail-simple-image"
+                    src={i8}
+                    alt
+                    width={270}
+                    height={200}
+                  />
+                  <div className="thumbnail-simple-caption">
+                    <span className="thumbnail-simple-price">$625</span>
+                    <span className="thumbnail-simple-persons">/person</span>
+                    <span className="thumbnail-simple-divider" />
+                    <span className="thumbnail-simple-days">7 days</span>
+                  </div>
+                </a>
+                <h5 className="thumbnail-simple-title">
+                  <a href="single-tour.html">Sri Lanka, India</a>
+                </h5>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default HotTours;
